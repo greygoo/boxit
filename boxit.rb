160d57bfa9d4676972f560ff9d34282150fbd95c
chuller <chuller@suse.de>
2016-08-02 15:52:11 +0200

    Fix output and add test command

--- boxit.rb
+++ boxit.rb
@@ -8,26 +8,25 @@ get '/' do
 end
 
 get '/pass' do
-  'Pass'
-  serialport.write('O')
+  serialport.write("O")
+  "Pass"
 end
 
 get '/fail' do
-  'Fail'
-  serialport.write('A')
+  serialport.write("A")
+  "Fail"
 end
 
 get '/switch_led/*/*' do |led_id, mode|
   led = led_id.to_i
   if led <= 10 && mode == "on" || mode == "off"
-   "Switching LED #{led_id} #{mode}"
    if mode == "on"
      command = "L"+led_id+"1"
    else
      command = "L"+led_id+"0"
    end
-   command
    serialport.write(command)
+   "Switching LED #{led_id} #{mode}"
   else 
    "Invalid values"
   end
@@ -36,10 +35,15 @@ end
 get '/move_gauge_to/*' do |gauge_position|
   position = gauge_position.to_i
   if position >= 0  && position <= 255
-    "Setting gauge to position #{position}"
     command = "G"+gauge_position
     serialport.write(command)
+    "Setting gauge to position #{position}"
   else
     "Position #{position} is invald"
   end
+end
+
+get '/test' do
+  serialport.write('T')
+  "Test"
 end
